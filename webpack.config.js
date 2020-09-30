3ce766f12830a6356532454442d65ea6dd5a644d
ARedondoVilla <alexredondovilla@gmail.com>
2020-09-30 18:25:49 +0000

    Rename .js entry file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
